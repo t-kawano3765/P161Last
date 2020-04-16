4aee8a778f2e5ef890e298dcfbae7ab94b8fba1c
t-kawano3765 <62410625+t-kawano3765@users.noreply.github.com>
2020-04-16 13:40:15 +0900

    fastVersion

---
+++ mainhiandblow.java
@@ -0,0 +1,17 @@
+package hiandblow;
+
+public class mainhiandblow {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		creatrandom cratrandom = new creatrandom();
+	
+		int rsno = cratrandom.creatRandom();
+				
+		System.out.println("");	
+		System.out.println(rsno);
+
+	}
+
+}
